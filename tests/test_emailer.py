2129d8ac61c67aea213c01b2f666951130b0c792
Cristian Calin <57292904+Soccolo@users.noreply.github.com>
2026-07-11 20:04:30 +0100

    Test inline formula rendering

---
+++ tests/test_emailer.py
@@ -0,0 +1,12 @@
+from briefing.emailer import render_formula_png
+
+
+def test_render_formula_png_embeds_a_png():
+    image = render_formula_png(r"p = \\frac{x}{n}")
+    assert image is not None
+    assert image.startswith(b"\x89PNG\r\n\x1a\n")
+
+
+def test_render_formula_png_returns_none_for_empty_formula():
+    assert render_formula_png("") is None
+
